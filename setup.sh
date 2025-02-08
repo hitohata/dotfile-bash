b57030ebff8fce6c558b37737965e66b9f352a70
hitohata <hirohatatro@gmail.com>
2025-02-07 21:36:27 -0800

    feat: updated the setup

--- setup.sh
+++ setup.sh
@@ -6,16 +6,25 @@ BASH_RC=${PWD}/.bashrc
 
 BACKUP_DIR="${HOME}/.DotenvBackups"
 
+
+# get this file dir
+script_path=$(readlink -f "$0")
+script_dir=$(dirname "$script_path")
+
+
 if [ ! -d $BACKUP_DIR ];
 then
     mkdir -p $BACKUP_DIR
 fi
 
+
+
 # .bashrc
 # remove .bashrc
 if [ -f ${HOME}/.bashrc ]; then
-    mv ${HOME}/.bashrc ${BACKUP_DIR}/.bashrc
+    mv ${HOME}/.bashrc ${script_dir}/.bashrc
+    rm ~/.bashrc
 fi
 
 # add symbolic link
-ln -s ${BASH_RC} ~/.bashrc
+ln -s "${script_dir}/.bashrc" ~/.bashrc
