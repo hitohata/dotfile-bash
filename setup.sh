161a6b31933ee9ca70b48ae53655eb3176131698
hitohata <hirohatatro@gmail.com>
2025-02-07 21:38:09 -0800

    fix

--- setup.sh
+++ setup.sh
@@ -1,29 +1,22 @@
 #/bin/bash
 
-BASH_RC=${PWD}/.bashrc
-
 # create backup directory
 
 BACKUP_DIR="${HOME}/.DotenvBackups"
-
 
 # get this file dir
 script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 
-
-if [ ! -d $BACKUP_DIR ];
-then
-    mkdir -p $BACKUP_DIR
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir -p $BACKUP_DIR
 fi
-
-
 
 # .bashrc
 # remove .bashrc
 if [ -f ${HOME}/.bashrc ]; then
-    mv ${HOME}/.bashrc ${script_dir}/.bashrc
-    rm ~/.bashrc
+  mv ${HOME}/.bashrc ${script_dir}/.bashrc
+  rm ~/.bashrc
 fi
 
 # add symbolic link
